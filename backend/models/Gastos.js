4a69874b5fbf37a7225e4f6a9dd605b92a6d980f
chuca1 <diego.gm99@hotmail.com>
2019-08-24 14:23:43 -0500

    gastos model

--- backend/models/Gastos.js
+++ backend/models/Gastos.js
@@ -2,11 +2,23 @@ const { model, Schema } = require("mongoose");
 
 const gastosSchema = new Schema(
   {
-    monto: Number,
-    descripcion: String,
-    categoria: String,
-    fecha: Date,
-    userid: Number
+    rfc: String,
+    uuid: String,
+    ccyisocode: String,
+    ccyfx: Number,
+    paymentmethod: Number,
+    paymenttype: String,
+    subtotal: Number,
+    total: Number,
+    placegenerated: Number,
+    date: Date,
+    receptorname: String,
+    emisorrfc: String,
+    emisorname: String,
+    status: String,
+    productid: Number,
+    quantity: Number,
+    cost: Number
   },
   {
     versionKey: false,
